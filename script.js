35a0fa00ed5e77fe0e44d3bddbef6c690eee4d16
Sosa Lucas <lucaszeta.2006@gmail.com>
2024-01-22 10:04:03 -0300

    Funcion Enter agregada

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ document.querySelector('#calcular2').addEventListener('click', ()=>{
     document.querySelector('#r2').innerHTML =r;
     document.getElementById('salida').innerHTML='El adicional en total es:';
 })
+var e = window.onkeydown;
+function Enter() {
+    if (e.keyCode == 13 && !e.shiftKey) {
+        console.log("anduvo")
+        e.preventDefault();
+        var boton = document.querySelector('#calcular2');
+        angular.element(boton).triggerHandler('click');
+    }
+}
+
+
+addEventListener("keydown", function(e) {
+
+    if (e.keyCode === 13 && !e.shiftKey) {
+        console.log("anduvo")
+        e.preventDefault();
+        const n1= parseInt(document.querySelector('#n11').value);
+    let r;
+    r = n1+(30*(n1/100))
+    document.querySelector('#r2').innerHTML =r;
+    document.getElementById('salida').innerHTML='El adicional en total es:';
+    }
+
+
+})
