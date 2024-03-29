ebe071c4298a4e4471d7c4f0199d620fb051261b
Sosa Lucas <lucaszeta.2006@gmail.com>
2024-03-29 13:49:55 -0300

    Update 29/03/2024
    Actualizacion 2.0 Adicional

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ document.querySelector('#calcular2').addEventListener('click', ()=>{
     const n1= parseInt(document.querySelector('#n11').value);
     if (n1 >= 1000){
     let r;
+    let u;
     r = n1+(150*(n1/1000));   
     document.querySelector('#r2').innerHTML =r;
     document.getElementById('salida').innerHTML='El Costo en total es:';
+
+    u = 150*(n1/1000);
+    document.querySelector('#r3').innerHTML =u;
+    document.getElementById('adicional').innerHTML='El Costo del Adicional es:';
+
+    console.log(u)
+
 }
 else{
     let r;
-    r = n1+(30*(n1/100))
+    let u;
+
+    r = n1+(30*(n1/100));
     
     document.querySelector('#r2').innerHTML =r;
     document.getElementById('salida').innerHTML='El Costo en total es:';
+
+    
+    u = 30*(n1/100);
+    document.querySelector('#r3').innerHTML =u;
+    document.getElementById('adicional').innerHTML='El Costo del Adicional es:';
+
+    console.log(u)
+
+
 }
 })
 var e = window.onkeydown;
