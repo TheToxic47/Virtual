97482004f36f2bc76780d630f3b62905f89206ff
Sosa Lucas <lucaszeta.2006@gmail.com>
2024-03-29 13:30:01 -0300

    Update 29/03/24
    Actualizar

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.querySelector('#calcular2').addEventListener('click', ()=>{
     const n1= parseInt(document.querySelector('#n11').value);
+    if (n1 >= 1000){
     let r;
-    r = n1+(30*(n1/100))
+    r = n1+(150*(n1/1000));   
     document.querySelector('#r2').innerHTML =r;
     document.getElementById('salida').innerHTML='El Costo en total es:';
+}
+else{
+    let r;
+    r = n1+(30*(n1/100))
+    
+    document.querySelector('#r2').innerHTML =r;
+    document.getElementById('salida').innerHTML='El Costo en total es:';
+}
 })
 var e = window.onkeydown;
 function Enter() {
